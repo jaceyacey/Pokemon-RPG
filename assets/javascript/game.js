e46586714ee03542722fa18336b002984f404537
Jace <cudnt_b_botherd@hotmail.com>
2019-10-08 10:29:23 +0800

    choose character and move unchosen to enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,45 @@
-const bulbasaur = $("#bulbasaur");
-const charmander = $("#charmander");
-const squirtle = $("#squirtle");
-const pikachu = $("#pikachu");
-let myPokemon;
+const pokemon = [
+  {
+    name: "Bulbasaur",
+    hp: 140,
+    attack: 8,
+    counter: 16
+  },
+  {
+    name: "Charmander",
+    hp: 120,
+    attack: 12,
+    counter: 24
+  },
+  {
+    name: "Squirtle",
+    hp: 160,
+    attack: 10,
+    counter: 20
+  },
+  {
+    name: "Pikachu",
+    hp: 100,
+    attack: 14,
+    counter: 28
+  }
+];
+
 let isPokemonChosen = false;
+
+//display characters to choose from
+$("#start-area").prepend("<h2>Your Character</h2>");
 
 //choose a character
 $(".pokemon").click(function() {
   if (isPokemonChosen) return;
-  myPokemon = this.id;
-  console.log(myPokemon);
   isPokemonChosen = true;
+  $("#enemies-area").html("<h2>Enemies Available to Attack</h2>");
+  $("#bulbasaur").appendTo("#enemies-area");
+  $("#charmander").appendTo("#enemies-area");
+  $("#squirtle").appendTo("#enemies-area");
+  $("#pikachu").appendTo("#enemies-area");
+  $(this).appendTo("#start-area");
 });
 
 //choose an opponent
